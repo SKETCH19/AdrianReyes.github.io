a0c8b7781c8e167a306daaea42c3259f95789f37
Adrian Reyes <phipps11x19@gmail.com>
2026-02-10 20:42:02 +0000

    4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://SKETCH19.github.io',
-  integration: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: false
-    })
-  ]
+  site: 'https://adrianreyes.dev'
 });
