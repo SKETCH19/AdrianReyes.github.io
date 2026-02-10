ec99f5f57583b49559714fe64399f2062f956a59
Adrian Reyes <phipps11x19@gmail.com>
2026-02-10 12:26:49 -0400

    Configure site URL and add integrations
    Added site URL and integrations for MDX, sitemap, and Tailwind CSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'http://AdrianReyes.github.io',
+  integration: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false
+    })
+  ]
+});
