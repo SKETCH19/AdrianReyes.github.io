f68c1a921ee3bab81b72db12d5c47e460575aeb8
Adrian Reyes <phipps11x19@gmail.com>
2026-02-10 12:44:57 -0400

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://AdrianReyes.github.io',
+  site: 'http://SKETCH19.github.io',
   integration: [
     mdx(),
     sitemap(),
